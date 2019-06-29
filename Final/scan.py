943513c1a27a0e3ef0686bbbff563bcdce2d81f3
LilyLiu0719 <Lily.ylliu@gmail.com>
2019-06-29 22:42:44 +0800

    rpi

--- Final/scan.py
+++ Final/scan.py
@@ -4,14 +4,14 @@ import picamera
 from PIL import Image
 import pyzbar.pyzbar as pyzbar
 import requests
-import lcddriver
-from time import *
+#import lcddriver
+import time
 import serial
 
-lcd = lcddriver.lcd()
-lcd.lcd_display_string("Welcome!", 1)
-lcd.lcd_display_string("Please show your QR code", 2)
-port = serial.Serial(“/dev/ttyAMA0″, baudrate=9600, timeout=0.5)
+#lcd = lcddriver.lcd()
+#lcd.lcd_display_string("Welcome!", 1)
+#lcd.lcd_display_string("Please show your QR code", 2)
+port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=0.5)
 
 while(True):
     ## take a photo and decode
@@ -34,17 +34,18 @@ while(True):
         print(tt)
         r = requests.get('http://' + tt)
         print('status = ', r.status_code)
-        lcd.lcd_display_string("QRcode recognized!", 3)
+        #lcd.lcd_display_string("QRcode recognized!", 3)
         if r.status_code == requests.codes.ok:
             print("connect OK!")
         else:
             print('failed to send request!')
-        print('text = ', r.texts[0])
-        port.write('1')
-        lcd.lcd_display_string("Hello" + r.texts[0], 4)
+        print(r.text)
+        port.write('r')
+        print('send char to arduino')
+        #lcd.lcd_display_string("Hello" + r.texts[0], 4)
     else: ## MORE THAN ONE QR CODE
         print('more than one QR code!')
-        lcd.lcd_display_string("One person at a time", 4)
+        #lcd.lcd_display_string("One person at a time", 4)
 
     # clean up
     del(pil)
